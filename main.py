a947d9b5a4c037d9e7b3b9725ea3ff462bf91110
Jack <77742409+JackFinnis@users.noreply.github.com>
2021-09-03 21:42:06 +0000

    Stage 1 details

--- main.py
+++ main.py
@@ -1,58 +1,71 @@
-from flask import Flask, jsonify
+import maintenance
 
-app = Flask(__name__)
+# from flask import Flask, jsonify
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     return 'API is working'
+
+# @app.route('/routes')
+# def get_routes():
+#     routeArray = []
+
+#     for i in range(1, 27):
+#         if i in range(1, 10):
+#             routeN = '0' + str(i)
+#         else:
+#             routeN = str(i)
+#         routePath = 'routes/route' + routeN
+
+#         routeDict = {}
+#         routeDict['id'] = i
+#         churchId = i * 50
+
+#         # Churches
+#         churchFile = open(routePath + '/churches.txt').read()
+#         churchData = []
+#         for church in churchFile.split('\n'):
+#             churchId += 1
+#             churchArray = church.split('|')
+#             churchDict = {
+#                 'id': churchId,
+#                 'name': churchArray[2],
+#                 'url': churchArray[3],
+#                 'coordinate': [float(churchArray[1]), float(churchArray[0])]
+#             }
+#             churchData.append(churchDict)
+#         routeDict['churches'] = churchData
+
+#         # Coordinates
+#         coordinatesFile = open(routePath + '/coordinates.txt').read()
+#         coordinatesData = []
+#         for coord in coordinatesFile.split('\n'):
+#             coordArray = coord.split('|')
+#             coord = [float(coordArray[1]), float(coordArray[0])]
+#             coordinatesData.append(coord)
+#         routeDict['coords'] = coordinatesData
+
+#         # Description
+#         descriptionFile = open(routePath + '/description.txt').read()
+#         descriptionData = descriptionFile.split('\n')
+#         routeDict['start'] = descriptionData[0]
+#         routeDict['end'] = descriptionData[1]
+#         routeDict['metres'] = int(descriptionData[2])
+
+#         if len(descriptionData) > 4:
+#             routeDict['routeTitle'] = descriptionData[4]
+#         if len(descriptionData) > 5:
+#             routeDict['routeDescription'] = descriptionData[5]
+#         if len(descriptionData) > 6:
+#             routeDict['directionsAuthor'] = descriptionData[6]
+#         if len(descriptionData) > 8:
+#             routeDict['directions'] = descriptionData[8:]
+
+#         routeArray.append(routeDict)
+#     return jsonify(routeArray)
 
 
-@app.route('/')
-def home():
-    return 'API is working'
-
-
-@app.route('/routes')
-def get_routes():
-    routeArray = []
-
-    for i in range(1, 27):
-        if i in range(1, 10):
-            routeN = '0' + str(i)
-        else:
-            routeN = str(i)
-        routePath = 'routes/route' + routeN
-
-        churchFile = open(routePath + '/churches.txt').read()
-        churchData = []
-        for church in churchFile.split('\n'):
-            churchArray = church.split('|')
-            churchDict = {
-                'name': churchArray[2],
-                'urlString': churchArray[3],
-                'coordinate': [float(churchArray[1]), float(churchArray[0])]
-            }
-            churchData.append(churchDict)
-
-        coordinatesFile = open(routePath + '/coordinates.txt').read()
-        coordinatesData = []
-        for coord in coordinatesFile.split('\n'):
-            coordArray = coord.split('|')
-            coord = [float(coordArray[1]), float(coordArray[0])]
-            coordinatesData.append(coord)
-
-        descriptionFile = open(routePath + '/description.txt').read()
-        descriptionData = descriptionFile.split('\n')
-        start = descriptionData[0]
-        end = descriptionData[1]
-        metres = int(descriptionData[2])
-
-        routeArray.append({
-            'id': i,
-            'start': start,
-            'end': end,
-            'metres': metres,
-            'churches': churchData,
-            'coords': coordinatesData
-        })
-
-    return jsonify(routeArray)
-
-
-app.run(host='0.0.0.0', port=8080, debug=True)
+# if __name__ == "__main__":
+#     from waitress import serve
+#     serve(app, host="0.0.0.0", port=8080)
--- maintenance.py
+++ maintenance.py
@@ -1,17 +1,22 @@
-
 # Get file extension
-# for i in range(2, 27):
-#     if i in range(1, 10):
-#         N = '0' + str(i)
-#     else:
-#         N = str(i)
-    
-#     # toWrite = toWrite[:-1]
-#     file = open(f'routes/route{N}/description.txt', "w")
-#     file.close()
+for i in range(1, 2):
+    if i in range(1, 10):
+        N = '0' + str(i)
+    else:
+        N = str(i)
+
+    toWrite = ""
+    churches = open(f'routes/route{N}/churches.txt').read().split('\n')
+    for i in range(len(churches)):
+        toWrite += churches[i] + "|" + str(i+1) + "\n"
+
+    toWrite = toWrite[:-1]
+    print(toWrite)
+    file = open(f'routes/route{N}/churches.txt', "w")
+    file.write(toWrite)
+    file.close()
+
 
 # Delete files
 # import os
 # os.remove('routes/route{0}/routeColour.txt'.format(N))
-
-# Write to files
